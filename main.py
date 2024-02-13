857d65e72fcb71a96417bd53aff91a476f8bea56
Morgan Molyneaux <92762025+MorganMolyneaux22@users.noreply.github.com>
2024-02-13 13:16:36 -0700

    IQR
    IQR range for Age and Height

--- main.py
+++ main.py
@@ -7,5 +7,16 @@ csv_data = pd.read_csv('cdc.csv')
 
 # Print the data
 plt.boxplot([csv_data["height"], csv_data["age"]], labels=["Height", "Age"])
+
+q1_height = csv_data['height'].quantile(0.25)
+q3_height = csv_data['height'].quantile(0.75)
+iqr_height = q3_height - q1_height
+print(f"IQR for Height: {iqr_height}")
+
+q1_age = csv_data['age'].quantile(0.25)
+q3_age = csv_data['age'].quantile(0.75)
+iqr_age = q3_age - q1_age
+print(f"IQR for Age: {iqr_age}")
+
 plt.show()
 
