1d6f1d6b0296a73816a3d5899bcb091f97305b74
Morgan Molyneaux <92762025+MorganMolyneaux22@users.noreply.github.com>
2024-02-13 13:30:04 -0700

    Adding comments
    Commenting code for ease of use.

--- main.py
+++ main.py
@@ -8,15 +8,23 @@ csv_data = pd.read_csv('cdc.csv')
 # Print the data
 plt.boxplot([csv_data["height"], csv_data["age"]], labels=["Height", "Age"])
 
+# Caclulate the IQR for height 
 q1_height = csv_data['height'].quantile(0.25)
 q3_height = csv_data['height'].quantile(0.75)
 iqr_height = q3_height - q1_height
 print(f"IQR for Height: {iqr_height}")
 
+# Caclulate the IQR for age
 q1_age = csv_data['age'].quantile(0.25)
 q3_age = csv_data['age'].quantile(0.75)
 iqr_age = q3_age - q1_age
 print(f"IQR for Age: {iqr_age}")
+
+# I think this is how we are able to find mean using the matplotlib libary. 
+test = csv_data["height"].mean()
+print(test)
+
+
 
 plt.show()
 
