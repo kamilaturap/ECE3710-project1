2d3ed9b2a00a39bf9e0b1d0619dad096222ab479
Morgan Molyneaux <92762025+MorganMolyneaux22@users.noreply.github.com>
2024-02-13 12:55:27 -0700

    Testing matplot LIbaray
    Importing and loading csv data into temp variable.  Need to shift through desired data, and create boxplot for quartile range now.

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-print("Hello world")
+import pandas as pd 
+import matplotlib.pyplot as plt
+
+
+# Load the data from CSV file into temp variable
+csv_data = pd.read_csv('cdc.csv')
+
+# Print the data
+plt.boxplot([csv_data["height"], csv_data["age"]], labels=["Height", "Age"])
+plt.show()
 
